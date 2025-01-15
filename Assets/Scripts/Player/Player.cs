beb53f9e3c934ef90c150a09db93a7de57d95349
Vadym Vatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2025-01-15 20:53:59 +0200

    Add wallJump

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -17,11 +17,13 @@ public class Player : MonoBehaviour
     [SerializeField] private float groundCheckDistance;
     [SerializeField] private float wallCheckDistance; // 2 - дистанция до стены
     [SerializeField] private LayerMask whatIsGround;
+    
     private bool _isGrounded;
     private bool _isAirborne; // в воздухе ли мы
     private bool _isWallDetected; // 1 - коснулись ли мы стены
     
     private float _xInput;
+    private float _yInput; // 15
     
     private bool _isFacingRight = true; // смотрит ли персонаж на право
     private int _facingDir = 1; // если смотрит в право (1), на лево (-1)
@@ -40,19 +42,32 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         UpdateAirBornStatus();
+        
+        // 10 - обязательно такой порядок вызовов
         HandleWallSlide(); // 5
-        HandleCollisions();
         HandleMovement();
-        HandleAnimations();
         HandleFlip(); // метод переворачивания персонажа
+        HandleCollisions();
+        HandleAnimations();
     }
 
     private void HandleWallSlide() // 6 - метод скольжения
     {
-        if (_isWallDetected && _rb.linearVelocity.y < 0)
-        {
-            _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, _rb.linearVelocity.y * 0.5f);
-        }
+        
+        bool canWallSlide = _isWallDetected && _rb.linearVelocity.y < 0; // 11 - локальная переменная, можно ли скользить
+        float yModifer = _yInput < 0? 1f : 0.05f; // 17 - модификатор скорости скольжения, если нажата кнопка вниз, то скорость модификатора 1
+        
+        if (!canWallSlide) return; // 12 - прекратить выполненение метода
+        
+        //if (_isWallDetected && _rb.linearVelocity.y < 0) // 13 - больше нам не нужно это условие
+        //{
+            // _rb.linearVelocity.y * 0.5f - потому что слад вниз должен быть медленным
+            //_rb.linearVelocity = new Vector2(_rb.linearVelocity.x, _rb.linearVelocity.y * 0.5f);
+        //}
+        
+        // _rb.linearVelocity.y * 0.5f - потому что слад вниз должен быть медленным
+        
+        _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, _rb.linearVelocity.y * yModifer); // 14
     }
 
     private void UpdateAirBornStatus() // переключатель состояния персонажа в воздухе
@@ -74,8 +89,9 @@ public class Player : MonoBehaviour
 
     private void HandleMovement()
     {
-        _rb.linearVelocity = new Vector2(_xInput * moveSpeed, _rb.linearVelocity.y);
+        if (_isWallDetected) return; // 8 - если прикоснулись к стене, то не двигаемся.
         
+        _rb.linearVelocity = new Vector2(_xInput * moveSpeed, _rb.linearVelocity.y);
     }
 
     private void HandleCollisions()
@@ -89,6 +105,7 @@ public class Player : MonoBehaviour
     private void HandleInput()
     {
         _xInput = Input.GetAxisRaw("Horizontal"); // GetAxisRaw - строго 1 или -1, тогда как GetAxis - плавает 
+        _yInput = Input.GetAxisRaw("Vertical"); // 16 - получаем вертикальное нажатие
         
         if (Input.GetKeyDown(KeyCode.Space)) JumpButton(); 
         
@@ -111,6 +128,7 @@ public class Player : MonoBehaviour
         _animator.SetFloat("xVelocity", _rb.linearVelocity.x); 
         _animator.SetFloat("yVelocity", _rb.linearVelocity.y); // анимация прыжка и падения
         _animator.SetBool("isGrounded", _isGrounded); // проверка земли, для выполнения анимации
+        _animator.SetBool("isWallDetected", _isWallDetected); // 7 - анимация скольжения
     }
 
     private void Jump() => _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, jumpForce);
@@ -121,9 +139,12 @@ public class Player : MonoBehaviour
         _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, doubleJumpForce);
     }
 
-    private void HandleFlip() // метод переворачивания
+    private void HandleFlip() // метод переворачивания 
     {
-        if (_rb.linearVelocity.x < 0 && _isFacingRight || _rb.linearVelocity.x > 0 && !_isFacingRight)
+        // 9 - тут нам нужно поменять условие, вместо if (_rb.linearVelocity.x < 0 && _isFacingRight || _rb.linearVelocity.x > 0 && !_isFacingRight)
+        // на (_xInput < 0 && _isFacingRight || _xInput > 0 && !_isFacingRight) - это для убирание бега при столкновении
+        // со стеной, что бы мы могли повернутся и идти в другую сторону
+        if (_xInput < 0 && _isFacingRight || _xInput > 0 && !_isFacingRight)
         {
             Flip();
         }
